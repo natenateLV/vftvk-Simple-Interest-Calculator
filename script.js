2543471806ec319092a2fce9720a8919d7350bdc
Nathaniel <nephillips1@gmail.com>
2022-05-18 21:47:36 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if (principal == "") { //Here Check is empty
+        alert("Amount can't be empty");
+        document.getElementById("principal").focus();
+        return false;
+    } else {
+        if (principal <= 0) { //Here check is a positive number
+            alert("Enter a positive number");
+            document.getElementById("principal").focus();
+            return false;
+        }
+    }
+
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    var result = "If you deposit " + principal + "</mark>, at an interest rate of " + rate + "%. You will receive an amount of " + interest + ", in the year " + year +"";
+    document.getElementById("result").innerHTML = result;
+
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
