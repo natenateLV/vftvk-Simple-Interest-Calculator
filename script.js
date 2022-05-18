26b083e665c59120567b2e633038634d506c511a
Nathaniel Phillips <79348370+natenateLV@users.noreply.github.com>
2022-05-18 15:24:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,3 @@ function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-
-
-
-If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + future + "</mark>"
